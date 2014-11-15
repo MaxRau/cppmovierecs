3f6e44077d3270a57a671fc70e312734e863e2f0
rau <max.rauchaplin@gmail.com>
2014-11-15 14:44:59 -0400

    Lazy commit deleting my film/person files

---
+++ test.cpp
@@ -0,0 +1,16 @@
+//Simple test of User, Person, Film Classes.
+#include "Person.cpp"
+#include "Film.cpp"
+#include "User.cpp"
+
+int main() {
+  //Define a movie (The Godfather)
+  Person Gwriter;
+  Person Gdirector;
+  Person actors[MAX_PEOPLE];
+  String characters[MAX_PEOPLE];
+  float rating = 4.5;
+  float earnings = 257000000;
+  Film Godfather;
+}
+
