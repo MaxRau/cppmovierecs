d645ad776e9f175534940771dbbcc8d92060310f
rau <max.rauchaplin@gmail.com>
2014-11-15 14:50:38 -0400

    Final merge commit

--- Person.cpp
+++ Person.cpp
@@ -1,15 +1,62 @@
-#include "Person.h"
-class Person {
-  Person::Person(Film c, String b, Gender g, int a)
-  {
-    contributedTo = c;
-    bio = b;
-    gender = g;
-    activeSince = a;
-  }
+#include "Film.h"
 
-  Film* getContributedTo() {return *contributedTo}
-  String getBio() {return bio}
-  Gender getGender() {return gender}
-  int getActiveSince() {return activeSince}
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+enum Gender {Male,Female};
+class Person{
+public:
+Person(Film contrubutedTo[], string bio, Gender gender, int activeSince);
+Film* getContributedTo();
+void addContributedTo(Film* film);
+string getBio();
+void setBio(string s);
+Gender getGender();
+void setGender(enum Gender);
+int getActiveSince();
+void setActiveSince(int y);
+private:
+Film contributedTo[];
+int numberContributedTo; //Unsure of this?
+string bio;
+Gender gender;
+int activeSince;
+};
+
+/*
+Person::Person(Film contrubutedTo[], string bio, Gender gender, int activeSince){
+    
+};
+*/
+void Person::setGender(Gender g){
+    gender = g;
+};
+Gender Person::getGender(){
+    return gender;
+};
+
+void Person::setBio(string s){
+   bio=s;
+};
+string Person::getBio(){
+    return bio;
+};
+
+void Person::setActiveSince(int as){
+   activeSince=as;
+};
+int Person::getActiveSince(){
+    return activeSince;
+};
+
+Film* Person::getContributedTo(){
+    return contributedTo;
+};
+/*
+void Person::addContributedTo(Film* film){
+    contributedTo[numberContributedTo++]=film;
 }
+ * */
--- Person.h
+++ Person.h
@@ -1,18 +1,33 @@
-class Person {
-  public:
-    Person(Film contrubutedTo, String bio, Gender gender, int activeSince);
-    
+
+#ifndef PERSON_H
+#define	PERSON_H
+#include "Person.h"
+#endif	/* PERSON_H */
+
+
+#ifndef FILM_H
+#define FILM_H
+#include "Film.h"
+#endif //FILM_H
+
+class Person{
+public:
+    Person(std::string bio, sex gender, int activeSince);
     Film* getContributedTo();
-    String getBio();
-    Gender getGender();
+    void addContributedTo(Film* film);
+    std::string getBio();
+    void setBio(std::string s);
+    sex getSex();
+    void setSex(enum sex);
     int getActiveSince();
-
-  private:
-    Film contributedTo[];
-    String bio;
-
-    enum Gender {Male,Female};
-    Gender gender;
-
+    void setActiveSince(int y);
+private:
+    std::list<Film*> contributedTo;
+    int nContributedTo; //Unsure of this?
+    std::string bio;
+    sex ex;
     int activeSince;
-}
+};
+
+
+
